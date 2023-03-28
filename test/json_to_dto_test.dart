5e1d1bb8645bef1a0d53b495846b2a6906e0c676
Christian Findlay <16697547+MelbourneDeveloper@users.noreply.github.com>
2023-03-28 13:12:10 +1100

    test the generated model

--- test/json_to_dto_test.dart
+++ test/json_to_dto_test.dart
@@ -3,20 +3,26 @@ import 'dart:convert';
 import 'package:json_to_dto/json_to_dto.dart';
 import 'package:test/test.dart';
 
+import 'person.dart';
+
 const jsonString =
     '{"name": "John", "age": 30, "isMarried": false, "height": 1.75, "children": [{"name": "Alice", "children" : [{"name":"Tim"}]}, {"name": "Bob"}], "address": {"street": "Main Street", "number": 123}}';
 
 void main() {
   group('A group of tests', () {
-    test('First Test', () {
+    test('Test Generation', () {
       final code = generateClasses(
           'Person', json.decode(jsonString) as Map<String, dynamic>);
       expect(code, example);
     });
+
+    test('Test Generated Model', () {
+      final person =
+          Person.fromJson(json.decode(jsonString) as Map<String, dynamic>);
+      expect(person.name, 'John');
+    });
   });
 }
-
-
 
 const example = '''
 
